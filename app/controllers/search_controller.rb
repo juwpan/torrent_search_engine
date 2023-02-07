1e4e8baf3f70c0e8324dae812c219fd9f04398d5
NathanielBampo <mergontv@gmail.com>
2023-02-07 15:04:38 +0400

    added search_controller and views for it

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,47 @@
+class SearchController < ApplicationController
+  before_action :set_search, only: [:show]
+  before_action :set_search_params, only: [:create]
+
+  def new
+    @search = Search.new(params[:query])
+  end
+  
+  def create
+    # if @search_value.present?
+    #   redirect_to search_path(@search_value.first.id)
+    # else
+    #   @search = Search.create_or_find_by(params_search)
+    #   if @search.save
+    #     redirect_to search_path(@search)
+    #   else
+    #     render :new
+    #   end
+    # end
+    if @search_value.present?
+      redirect_to search_index_path
+    else
+      render :new
+    end
+  end
+
+  def show
+  end
+
+  def index
+    @searches = Search.joins(:torrent_links).select("torrent_links.*").paginate(page: params[:page], per_page: 30)
+  end
+
+  private
+
+  def set_search_params
+    @search_value = Search.where("query = ?", (params[:search][:query]).downcase)
+  end
+
+  def set_search
+    @search = Search.find(params[:id])
+  end
+
+  def params_search
+    params.require(:search).permit(:query)
+  end
+end
---
+++ app/helpers/search_helper.rb
@@ -0,0 +1,17 @@
+module SearchHelper
+  def color_link_seed(seed)
+    if seed > 0
+      content_tag :span, seed, class: "text-success"
+    else
+      content_tag :span, seed, class: "text-danger"
+    end
+  end
+
+  def color_link_lychee(lychee)
+    if lychee > 0
+      content_tag :span, lychee, class: "text-primary"
+    else
+      content_tag :span, lychee, class: "text-danger"
+    end
+  end
+end
