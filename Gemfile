abfca770e5f3dae71c684eaad52694296d183528
NathanielBampo <mergontv@gmail.com>
2023-02-13 16:28:07 +0400

    change BD(SQL on PostgresQl)

--- Gemfile
+++ Gemfile
@@ -11,16 +11,15 @@ gem "turbo-rails"
 gem "stimulus-rails"
 gem "cssbundling-rails"
 gem "redis", "~> 4.0"
-# gem "bcrypt", "~> 3.1.7"
 gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "bootsnap", require: false
 gem "sassc-rails"
 
 gem 'mechanize'
 gem 'will_paginate', '~> 3.3'
+gem 'pg', '~>1.4.5'
 
 group :development, :test do
-  gem "sqlite3", "~> 1.4"
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
   gem 'dotenv-rails'
 end
@@ -28,10 +27,4 @@ end
 group :development do
   gem 'bullet'
   gem "web-console"
-  # gem "rack-mini-profiler"
-  # gem "spring"
-end
-
-group :production do
-  gem 'pg'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_10_113350) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_13_122554) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "searches", force: :cascade do |t|
     t.string "query"
     t.datetime "created_at", null: false
@@ -19,7 +22,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_10_113350) do
   end
 
   create_table "torrent_links", force: :cascade do |t|
-    t.integer "search_id"
+    t.bigint "search_id"
     t.string "name"
     t.string "link"
     t.string "link_forum"
